b291d38cd3f282185584878aca6c18b599bc1b5a
Chr0527 <1015670600@qq.com>
2019-06-28 11:46:11 +0800

    aa

--- src/main/java/com/lnsoft/SpringDataJpaApplication.java
+++ src/main/java/com/lnsoft/SpringDataJpaApplication.java
@@ -2,13 +2,20 @@ package com.lnsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringDataJpaApplication {
+public class SpringDataJpaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaApplication.class, args);
 	}
 
+	@Override//为了打包springboot项目
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
 }
 
